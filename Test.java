70eebaf1c075c1564b09445319123b06af6c7005
Kevin S <Kevin.Sommer91@freenet>
2021-12-17 14:25:36 +0100

    Test.java hinzugefuegt

---
+++ Test.java
@@ -0,0 +1,10 @@
+class Test{
+    public static void main(String[] args){
+        int i = 3;
+        i++;
+        int j = 5;
+        j++;
+        System.out.println(i);
+        System.out.println(j);
+    }
+}
